5232fd59767375876c4860c5bdceba90f736e0c8
Trictonicmp <code_shadow@hotmail.com>
2022-04-05 00:26:10 -0500

    Fix linter errors

--- app.js
+++ app.js
@@ -3,36 +3,37 @@ const closeBtn = document.getElementById('closebtn');
 const icon = document.querySelector('i');
 const xBtn = document.getElementById('closebtn');
 
-
 let openmenu = true;
 
-
-icon.addEventListener('click', function (evt) {
-    if (!openmenu) {
-        openmenu = true;
-        hiddenNav.style = 'display: none;';
-        hiddenNav.classList.toggle('showNav');
-    } else {
-        openmenu = false;
-        hiddenNav.style = 'display: flex;';
-        hiddenNav.classList.toggle('showNav');
-        xBtn.classList.remove('xBtn');
-        xBtn.classList.add('xBtnStyle');
-        closeBtn.style = 'display: flex;'
-    }
+icon.addEventListener('click', () => {
+  if (!openmenu) {
+    openmenu = true;
+    hiddenNav.style = 'display: none;';
+    hiddenNav.classList.toggle('showNav');
+  } else {
+    openmenu = false;
+    hiddenNav.style = 'display: flex;';
+    hiddenNav.classList.toggle('showNav');
+    xBtn.classList.remove('xBtn');
+    xBtn.classList.add('xBtnStyle');
+    closeBtn.style = 'display: flex;';
+  }
 });
 
-xBtn.addEventListener('click', function (evt) {
-    if (openmenu = true) {
-        hiddenNav.style = 'display: none;';
-        hiddenNav.classList.toggle('showNav');
-    }
+xBtn.addEventListener('click', () => {
+  if (openmenu === false) {
+    openmenu = true;
+    hiddenNav.style = 'display: none;';
+    hiddenNav.classList.toggle('showNav');
+  }
 });
 
 function redirect() {
-    if (openmenu = true) {
-        let anchors = document.getElementsByClassName('redirect');
-        hiddenNav.style = 'display: none;';
-        hiddenNav.classList.toggle('showNav');
-    }
-};
+  if (openmenu === false) {
+    openmenu = true;
+    hiddenNav.style = 'display: none;';
+    hiddenNav.classList.toggle('showNav');
+  }
+}
+
+redirect();
