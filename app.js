cd957715f7ef3721734e35f68d667d07d022f572
jr-cast <castanos@pm.me>
2022-04-06 13:58:05 -0500

    add dynamic generation of cards and fix text overflowing issue

--- app.js
+++ app.js
@@ -38,60 +38,113 @@ function redirect() {
 
 const projectsStore = [
   {
+    'card-img': 'static/card1.jpg',
     'det-img': 'static/SnapshootPortfolio.png',
     'det-img-alt': 'main project image',
     'det-header': 'Keeping track of hundreds of components',
-    'det-list-cont': ['CodeKit', 'GitHub', 'JavaScript', 'Docker', 'x1'],
+    'det-list-cont': ['CodeKit', 'GitHub', 'JavaScript', 'Card1'],
     'det-btn-live': 'sampleProject1.com',
     'det-btn-source': 'sampleSource1.com',
     'det-description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
   },
   {
+    'card-img': 'static/card2.jpg',
     'det-img': 'static/SnapshootPortfolio.png',
     'det-img-alt': 'main project image',
     'det-header': 'Keeping track of hundreds of components twice',
-    'det-list-cont': ['CodeKit', 'GitHub', 'JavaScript', 'Docker', '2nd Card'],
+    'det-list-cont': ['CodeKit', 'GitHub', 'JavaScript', 'Card2'],
     'det-btn-live': 'sampleFifthProject2.com',
     'det-btn-source': 'sampleSource2.com',
     'det-description': 'Twice Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
   },
   {
+    'card-img': 'static/card3.jpg',
     'det-img': 'static/SnapshootPortfolio.png',
     'det-img-alt': 'main project image',
     'det-header': 'Keeping track of hundreds of components thrice',
-    'det-list-cont': ['CodeKit', 'GitHub', 'JavaScript', 'Docker', '3rd Card'],
+    'det-list-cont': ['CodeKit', 'GitHub', 'JavaScript', 'Card3'],
     'det-btn-live': 'sampleProject3.com',
     'det-btn-source': 'sampleSource3.com',
     'det-description': 'Thrice Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
   },
   {
+    'card-img': 'static/card1.jpg',
     'det-img': 'static/SnapshootPortfolio.png',
     'det-img-alt': 'main project image',
     'det-header': 'Keeping track of hundreds of components Fourth',
-    'det-list-cont': ['CodeKit', 'GitHub', 'JavaScript', 'Docker', '4th Card'],
+    'det-list-cont': ['CodeKit', 'GitHub', 'JavaScript', 'Card4'],
     'det-btn-live': 'sampleProject4.com',
     'det-btn-source': 'sampleSource4.com',
     'det-description': 'Fourth Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
   },
   {
+    'card-img': 'static/card2.jpg',
     'det-img': 'static/SnapshootPortfolio.png',
     'det-img-alt': 'main project image',
     'det-header': 'Keeping track of hundreds of components Fifth',
-    'det-list-cont': ['CodeKit', 'GitHub', 'JavaScript', 'Docker', '5th Card'],
+    'det-list-cont': ['CodeKit', 'GitHub', 'JavaScript', 'Card5'],
     'det-btn-live': 'sampleProject5.com',
     'det-btn-source': 'sampleSource5.com',
     'det-description': 'Fifth Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
   },
   {
+    'card-img': 'static/card1.jpg',
     'det-img': 'static/SnapshootPortfolio.png',
     'det-img-alt': 'main project image',
     'det-header': 'Keeping track of hundreds of components Sixth',
-    'det-list-cont': ['CodeKit', 'GitHub', 'JavaScript', 'Docker', '6th Card'],
+    'det-list-cont': ['CodeKit', 'GitHub', 'JavaScript', 'Card6'],
     'det-btn-live': 'sampleProject6.com',
     'det-btn-source': 'sampleSource6.com',
     'det-description': 'Sixth Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
   },
 ];
+
+// Create cards dynamically with for loop
+for (let i = 0; i < projectsStore.length; i++) {
+  let card = document.createElement('div');
+  card.className = 'card1';
+  card.id = `card${i}`;
+  let imgCont = document.createElement('div');
+  imgCont.className = 'img';
+  imgCont.id = `imgCont${i}`;
+  let img = document.createElement('img');
+  img.className = 'img1';
+  img.src = (projectsStore[i])["card-img"];
+  let cardFooter = document.createElement('div');
+  cardFooter.className = 'cardFooter';
+  cardFooter.id = `cardFooter${i}`;
+  let footerTitle = document.createElement('div');
+  footerTitle.className = 'footerTitle';
+  footerTitle.id = `footerTitle${i}`;
+  let title = document.createElement('h3');
+  title.className = 'cardHeader';
+  title.id = `title${i}`;
+  let ul = document.createElement('ul');
+  ul.className = 'tags';
+  ul.id = `ul${i}`;
+  for (let j = 0; j < projectsStore[i]["det-list-cont"].length; j += 1) {
+    let detli = document.createElement('li');
+    detli.innerHTML = projectsStore[i]["det-list-cont"][j];
+   ul.appendChild(detli);
+  }
+  let cardButton = document.createElement('button');
+  cardButton.className = 'cardBtn';
+  cardButton.id = `cardBtn${i}`;
+  cardButton.innerHTML = 'See Project'
+  cardButton.onclick = function openDetails() {
+    detailsAction(i);
+  }
+
+  title.innerHTML = (projectsStore[i])["det-header"];
+  document.getElementById('cardCont').appendChild(card);
+  document.getElementById(`card${i}`).appendChild(imgCont);
+  document.getElementById(`imgCont${i}`).appendChild(img);
+  document.getElementById(`imgCont${i}`).appendChild(cardFooter);
+  document.getElementById(`cardFooter${i}`).appendChild(footerTitle);
+  document.getElementById(`footerTitle${i}`).appendChild(title);
+  document.getElementById(`cardFooter${i}`).appendChild(ul);
+  document.getElementById(`cardFooter${i}`).appendChild(cardButton);
+}
 
 function detailsAction(id) {
   const mainDetails = document.getElementById('mainDetails');
